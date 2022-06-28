a382e431a67fbd448af9c3bf7c1442c484345ac4
smallpig01 <smallpig01@gmail.com>
2022-06-28 11:22:33 +0800

    feat✨(app): use opencv and capture webcam

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 
+#include "opencv2/opencv.hpp"
+
 using namespace std;
+using namespace cv;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    // webcam setting
+    const int deviceID = 2;
+    const int apiID    = cv::CAP_ANY;
+    VideoCapture cap;
+    cap.open(deviceID, apiID);
+    if (!cap.isOpened())
+    {
+        std::cout << "ERROR! Unable to open camera\n";
+        exit(-1);
+    }
+
+    // capture webcap
+    Mat MyMatColor;
+    while(1)
+    {
+        cap.read(MyMatColor);
+        cv::imshow( "MyMatColor", MyMatColor );
+        cv::waitKey(30);
+    }
+
     return 0;
 }
